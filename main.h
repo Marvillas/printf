11cf3955b7eeb67fa45904d2148a9072d4468b82
Marvis Aigbe <agelv4real@gmail.com>
2023-04-22 17:18:46 +0100

    added main.h

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+/* functions handling format characters c, s % */
+int print_char(va_list types, int flags);
+int print_string(va_list types, int flags);
+int print_percent(va_list types, int flags);
+
+#endif /* MAIN_H */
